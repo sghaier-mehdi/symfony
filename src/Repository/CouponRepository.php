cd4cc108873eb09e03192fc24b58271a10ec5739
bgt94 <bougattayamehdi1@gmail.com>
2025-05-12 01:09:46 +0200

    CouponRepository

---
+++ src/Repository/CouponRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Coupon;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Coupon>
+ *
+ * @method Coupon|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Coupon|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Coupon[]    findAll()
+ * @method Coupon[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CouponRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Coupon::class);
+    }
+
+    public function save(Coupon $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Coupon $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * @return Coupon[] Returns an array of active Coupon objects
+     */
+    public function findActiveCoupons(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.isActive = :active')
+            ->andWhere('c.expirationDate > :now')
+            ->setParameter('active', true)
+            ->setParameter('now', new \DateTime())
+            ->orderBy('c.expirationDate', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+} 
