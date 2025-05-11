2d6476d07b1bdf459a7e99dbaa8669fbe21ce332
bgt94 <bougattayamehdi1@gmail.com>
2025-05-12 01:07:25 +0200

    couponController

---
+++ src/Controller/CouponController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Coupon;
+use App\Form\CouponType;
+use App\Repository\CouponRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/coupon')]
+class CouponController extends AbstractController
+{
+    #[Route('/', name: 'app_coupon_index', methods: ['GET'])]
+    public function index(CouponRepository $couponRepository): Response
+    {
+        return $this->render('coupon/index.html.twig', [
+            'coupons' => $couponRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_coupon_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, CouponRepository $couponRepository): Response
+    {
+        $coupon = new Coupon();
+        $coupon->setExpirationDate(new \DateTime('+1 month')); // Set default expiration date
+        $form = $this->createForm(CouponType::class, $coupon);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $couponRepository->save($coupon, true);
+
+            $this->addFlash('success', 'Coupon created successfully');
+            return $this->redirectToRoute('app_coupon_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('coupon/new.html.twig', [
+            'coupon' => $coupon,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_coupon_show', methods: ['GET'])]
+    public function show(Coupon $coupon): Response
+    {
+        return $this->render('coupon/show.html.twig', [
+            'coupon' => $coupon,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_coupon_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Coupon $coupon, CouponRepository $couponRepository): Response
+    {
+        $form = $this->createForm(CouponType::class, $coupon);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $couponRepository->save($coupon, true);
+
+            $this->addFlash('success', 'Coupon updated successfully');
+            return $this->redirectToRoute('app_coupon_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('coupon/edit.html.twig', [
+            'coupon' => $coupon,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_coupon_delete', methods: ['POST'])]
+    public function delete(Request $request, Coupon $coupon, CouponRepository $couponRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$coupon->getId(), $request->request->get('_token'))) {
+            $couponRepository->remove($coupon, true);
+            $this->addFlash('success', 'Coupon deleted successfully');
+        }
+
+        return $this->redirectToRoute('app_coupon_index', [], Response::HTTP_SEE_OTHER);
+    }
+} 
